320c80a572019822b38871cbfcc986114af109fa
Derek Sifford <dereksifford@gmail.com>
2023-06-18 13:51:34 -0400

    misc updates

--- .chezmoiscripts/run_after_dump-brewfile.sh
+++ .chezmoiscripts/run_after_dump-brewfile.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 if command -v brew > /dev/null; then
-    brew bundle dump --describe --force --file=~/.local/share/chezmoi/.data/Brewfile
+    brew bundle dump --describe --force --brews --taps --casks --file=~/.local/share/chezmoi/.data/Brewfile
 fi
 
